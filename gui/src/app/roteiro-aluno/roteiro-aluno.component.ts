5af44b19873ef1250e9cfaf5de13622138815b35
Eden Santana <eeas@cin.ufpe.br>
2018-12-09 23:02:20 -0300

    added roteiro aluno

---
+++ gui/src/app/roteiro-aluno/roteiro-aluno.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roteiro-aluno',
+  templateUrl: './roteiro-aluno.component.html',
+  styleUrls: ['./roteiro-aluno.component.css']
+})
+export class RoteiroAlunoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
