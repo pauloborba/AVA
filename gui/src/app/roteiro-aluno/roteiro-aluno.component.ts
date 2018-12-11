5d8d6335bb31df466816fd86cafe76207567568b
Matheus Borba <mlbas@cin.ufpe.br>
2018-12-11 07:48:33 -0300

    Refatoração de métodos

--- gui/src/app/roteiro-aluno/roteiro-aluno.component.ts
+++ gui/src/app/roteiro-aluno/roteiro-aluno.component.ts
@@ -45,9 +45,7 @@ export class RoteiroAlunoComponent implements OnInit {
           return i;
         }
       }
-    }
-
-    else{
+    }else{
       //Proxima questão existe e é "branco"
       if(this.questoesRespondidas[questaoAtual+1].status == undefined){
         //Vai pra próxima questão
@@ -76,31 +74,26 @@ export class RoteiroAlunoComponent implements OnInit {
     }
     return -1;
   }
+  //refatoração feita por borba
 
-  // Cenario 1 e 2 de Guila
-  public giveupQuestion(modo:string){
+  //cenario 1 e 2 de Guila & 3 e 4 de Borba
 
-    this.questoesRespondidas[this.questaoAtual].status = Status.Desistida;
-    this.matriculaService.addQuestaoRespondida(this.cpfAluno, this.turmaId, this.roteiroId, this.questaoAtual, this.questoesRespondidas[this.questaoAtual]);
+  public changeQuestion(modo:string){
+    if(modo == 'Desistida'){
+      this.questoesRespondidas[this.questaoAtual].status = Status.Desistida;
+      this.matriculaService.addQuestaoRespondida(this.cpfAluno, this.turmaId, this.roteiroId, this.questaoAtual, this.questoesRespondidas[this.questaoAtual]);
+    }else{
+      this.questoesRespondidas[this.questaoAtual].status = Status.Pendente;
+      this.matriculaService.addQuestaoRespondida(this.cpfAluno, this.turmaId, this.roteiroId, this.questaoAtual, this.questoesRespondidas[this.questaoAtual]);
+    }
+
     
     let proximaQuestao = this.getProxima(this.questaoAtual);
     if(proximaQuestao == -1){
       this.questaoAtual = 1;
     }else{
       this.questaoAtual = proximaQuestao;
-    }
-  }
-
-  public askQuestion(modo:string){
-    this.questoesRespondidas[this.questaoAtual].status = Status.Pendente;
-    this.matriculaService.addQuestaoRespondida(this.cpfAluno, this.turmaId, this.roteiroId, this.questaoAtual, this.questoesRespondidas[this.questaoAtual]);
-
-    let proximaQuestao = this.getProxima(this.questaoAtual);
-    if(proximaQuestao == -1){
-      this.questaoAtual = 1;
-    }else{
-      this.questaoAtual = proximaQuestao;
-    }
+    } 
   }
 
 
