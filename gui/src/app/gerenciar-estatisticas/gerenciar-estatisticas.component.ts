5432d0bd9a01e8f7723bbe9f616397d65abc4c62
mvgmb <mvgmb@cin.ufpe.br>
2018-12-11 05:17:06 -0300

    Implemented Feature Numero de acertos por turma

--- gui/src/app/gerenciar-estatisticas/gerenciar-estatisticas.component.ts
+++ gui/src/app/gerenciar-estatisticas/gerenciar-estatisticas.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TurmaService } from '../shared/service/turma.service';
+import { MatriculaService } from '../shared/service/matricula.service';
+import { Meta } from '../shared/meta.model';
+import { QuestaoRespondida } from '../shared/questaoresponida.model';
+import { RoteiroService } from '../shared/service/roteiro.service';
 
 @Component({
   selector: 'app-gerenciar-estatisticas',
@@ -8,13 +13,62 @@ import { Component, OnInit, Input } from '@angular/core';
 export class GerenciarEstatisticasComponent implements OnInit {
 
   @Input()
-  private cpfAtual:string;
+  private cpfAtual: string;
   @Input()
-  private turmaAtual:string;
+  private turmaAtual: string;
   
-  constructor() { }
+  private roteirosId: string[];
+  private nomeRoteiros: Map<string,string>;
+  private noQuestoesRoteiro: Map<string, number>;
+  private alunosCpf: string[];
+  private noAcertos: Map<string, number>;
 
-  ngOnInit() {
+  constructor(
+    private turmaService: TurmaService,
+    private matriculaService: MatriculaService,
+    private roteiroService: RoteiroService,
+  )
+  {
+    this.roteirosId = [];
+    this.noQuestoesRoteiro = new Map<string, number>();
+    this.nomeRoteiros = new Map<string,string>();
+    this.alunosCpf = [];
+    this.noAcertos = new Map<string, number>();
   }
 
+  ngOnInit() {
+    this.turmaService.getTurma(this.turmaAtual).then(turma => {
+      // Get all alunos cpf
+      turma.alunos.forEach(aluno => {
+        this.alunosCpf.push(aluno.cpf);
+      });
+      // Get all roteiros id
+      turma.roteiros.forEach(roteiro => {
+        this.roteirosId.push(roteiro.id);
+        this.roteiroService.getRoteiro(roteiro.id)
+        .then(r => {
+          // Get number of questions
+          this.nomeRoteiros[roteiro.id] = r.nome;
+          this.noQuestoesRoteiro[roteiro.id] = r.noQuestoes;
+        });
+      });
+    })
+    .then(() => {
+      this.alunosCpf.forEach(cpf => {
+        this.roteirosId.forEach(roteiroId => {
+          this.matriculaService.getQuestoesRespondidas(cpf, this.turmaAtual, roteiroId)
+          .then(questoesRespondidas => {
+            for (var i = 1; i <= this.noQuestoesRoteiro[roteiroId]; i++) {
+              if (questoesRespondidas[i].nota === Meta.MPA || questoesRespondidas[i].nota === Meta.MA) {
+                if (this.noAcertos[roteiroId]) {
+                  this.noAcertos[roteiroId]++;
+                }
+                else this.noAcertos[roteiroId] = 1;
+              }
+            }
+          });
+        });
+      });
+    });
+  }
 }
