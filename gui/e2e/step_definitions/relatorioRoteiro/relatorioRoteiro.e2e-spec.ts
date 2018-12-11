2b67a5bff45757f6b31d7a6582fa663734797257
Éden Santana <eeas@cin.ufpe.br>
2018-12-11 19:01:08 -0300

    Added tests: Feature questoes certas

--- gui/e2e/step_definitions/relatorioRoteiro/relatorioRoteiro.e2e-spec.ts
+++ gui/e2e/step_definitions/relatorioRoteiro/relatorioRoteiro.e2e-spec.ts
@@ -20,6 +20,9 @@ defineSupportCode(function ({ Given, When, Then }) {
   Given(/^a turma obteve o percentual de acerto de "([^\"]*)"% para o roteiro de "([^\"]*)"/, (cpf) => {
     // auxpage.logAtTurmaPageAs(cpf.toString());
   });
+  Given('O aluno {string} acertou {string} questões', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+  });
   
   When('Eu acesso a turma {string}', function (string) {
     // Write code here that turns the phrase above into concrete actions
@@ -27,8 +30,11 @@ defineSupportCode(function ({ Given, When, Then }) {
   When('Eu acesso {string} da turma', function (string) {
       // Write code here that turns the phrase above into concrete actions
   });
+  When('Eu acesso o aluno {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+  });
   
-  Then('And Eu posso ver o percentual de acerto de {string}% para o roteiro de "{string}"', function (string, string2, string3, string4) {
+  Then('And Eu posso ver a {string} igual a {string}', function (string3, string4) {
       // Write code here that turns the phrase above into concrete actions
   });
 });
