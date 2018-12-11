8aa0bf9142d0710d5034eb2a8341712d43a4c1dc
mvgmb <mvgmb@cin.ufpe.br>
2018-12-11 06:10:45 -0300

    Added Feature numero de perguntas da turma em um roteiro tests

---
+++ gui/e2e/step_definitions/noPerguntasTurma/noPerguntasTurma.e2e-spec.ts
@@ -0,0 +1,36 @@
+import { defineSupportCode } from 'cucumber';
+import { AuxPage } from './noPerguntasTurma.po';
+import { browser } from 'protractor';
+import { Http } from '@angular/http';
+import { async } from 'q';
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+chai.should();
+const expect = chai.expect;
+
+const auxpage = new AuxPage();
+
+defineSupportCode(function ({ Given, When, Then }) {
+  
+  Given(/^Eu estou na página de "([^\"]*)"/, (page) => {
+    // browser.get("http://localhost:4200/login");
+  });
+  Given(/^Eu estou logado com o CPF "([^\"]*)"/, (cpf) => {
+    // auxpage.logAtTurmaPageAs(cpf.toString());
+  });
+
+  Given(/^A turma "([^\"]*)" teve "([^\"]*)" perguntas no roteiro de "([^\"]*)" com ID "([^\"]*)"/, (turmaId, noperguntas, nomeRoteiro, roteiroId) => {
+    // auxpage.getTurmas(turmaId);
+  });
+  When('Eu acesso a turma {string}', function (string) {
+      // Write code here that turns the phrase above into concrete actions
+  });
+  When('Eu acesso {string} da turma {string}', function (string, string2) {
+      // Write code here that turns the phrase above into concrete actions
+  });
+  Then('Eu consigo ver que a turma {string} teve {string} perguntas no roteiro de {string}com ID {string}', function (string, string2, string3, string4) {
+      // Write code here that turns the phrase above into concrete actions
+  });
+});
