0837fc43db820731512cb8b3eac01d0e18e598fb
claudiocarvalhoo <cco2@cin.ufpe.br>
2018-12-11 23:52:51 -0300

    added percentual de desistencias in roteiro

--- gui/src/app/estatistica-questao/estatistica-questao.component.ts
+++ gui/src/app/estatistica-questao/estatistica-questao.component.ts
@@ -3,6 +3,7 @@ import { QuestaoRespondida } from '../shared/questaoresponida.model';
 import { RoteiroService} from '../shared/service/roteiro.service';
 import {TurmaService} from '../shared/service/turma.service';
 import { Pessoa } from '../shared/pessoa.model';
+import { Questao } from '../shared/questao.model';
 
 @Component({
   selector: 'app-estatistica-questao',
@@ -13,18 +14,17 @@ export class EstatisticaQuestaoComponent implements OnInit {
 
 
   @Input()
-  roteiroId:string;
-  @Input()
-  turmaId:string;
+  id:string;
 
   private questoesRoteiro:QuestaoRespondida[];
+  private questoesDesseRoteiro:Map<number, Questao>;
   private alunos:Pessoa[];
+  private questoes:Questao[];
 
   constructor(
     private roteiroService:RoteiroService,
     private turmaService:TurmaService,
   ) { }
-
 
   pegarTempoMedio(){
     /*
@@ -39,7 +39,17 @@ export class EstatisticaQuestaoComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.questoes = new Array<Questao>();
+    this.roteiroService.getQuestoes(this.id).then(value =>{
+        this.questoesDesseRoteiro = value;
 
+    for(var i =0;i<4;i++){
+      this.questoes[i] = new Questao;
+      this.questoes[i] = this.questoesDesseRoteiro[i];
+    }
+    })
+
+    
     // Pegar as respostas de um roteiro pelo id dele e da turma, armazenar no array de questao respondida
 
     
