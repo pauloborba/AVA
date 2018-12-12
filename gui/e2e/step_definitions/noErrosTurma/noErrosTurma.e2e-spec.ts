d73b466ba6610e6ddc57d619a478cae659726e1f
mvgmb <mvgmb@cin.ufpe.br>
2018-12-11 21:56:47 -0300

    Added tests to the Feature Numero de erros por turma

--- gui/e2e/step_definitions/noErrosTurma/noErrosTurma.e2e-spec.ts
+++ gui/e2e/step_definitions/noErrosTurma/noErrosTurma.e2e-spec.ts
@@ -4,33 +4,35 @@ import { browser } from 'protractor';
 import { Http } from '@angular/http';
 import { async } from 'q';
 
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-chai.should();
-const expect = chai.expect;
-
-const auxpage = new AuxPage();
-
 defineSupportCode(function ({ Given, When, Then }) {
-  
-  Given(/^Eu estou na página de "([^\"]*)"/, (page) => {
-    // browser.get("http://localhost:4200/login");
-  });
-  Given(/^Eu estou logado com o CPF "([^\"]*)"/, (cpf) => {
-    // auxpage.logAtTurmaPageAs(cpf.toString());
-  });
+    const chai = require('chai');
+    const chaiAsPromised = require('chai-as-promised');
+    chai.use(chaiAsPromised);
+    chai.should();
+    const expect = chai.expect;
 
-  Given(/^A turma "([^\"]*)" teve "([^\"]*)" erros no roteiro de "([^\"]*)" com ID "([^\"]*)"/, (turmaId, noAcertos, nomeRoteiro, roteiroId) => {
-    // auxpage.getTurmas(turmaId);
-  });
-  When('Eu acesso a turma {string}', function (string) {
-      // Write code here that turns the phrase above into concrete actions
-  });
-  When('Eu acesso {string} da turma {string}', function (string, string2) {
-      // Write code here that turns the phrase above into concrete actions
-  });
-  Then('Eu consigo ver que a turma {string} teve {string} erros no roteiro de {string}com ID {string}', function (string, string2, string3, string4) {
-      // Write code here that turns the phrase above into concrete actions
-  });
+    const auxpage = new AuxPage();
+
+    // Given(/^Eu estou na página de "([^\"]*)"/, (page) => {
+    //     auxpage.navigateTo(`${page}`);
+    // });
+
+    // Given(/^Eu estou logado com o CPF "([^\"]*)"/, (cpf) => {
+    //     auxpage.logAs(cpf.toString());
+    // });
+
+    Given(/^A turma "([^\"]*)" teve "([^\"]*)" erros no roteiro de "([^\"]*)" com ID "([^\"]*)"/, (turmaId, noperguntas, nomeRoteiro, roteiroId) => {
+    });
+
+    // When(/^Eu acesso a turma "([^\"]*)"/, function (turma) {
+    //     auxpage.acessoTurma(`${turma}`);
+    // });
+
+    // When(/^Eu acesso "([^\"]*)" da turma "([^\"]*)"/, function (botao, turma) {
+    //     auxpage.estatisticasButton().click();
+    // });
+
+    Then(/^Eu consigo ver que a turma "([^\"]*)" teve "([^\"]*)" erros no roteiro de "([^\"]*)" com ID "([^\"]*)"/, function (turma, perguntas, roteiro, id) {
+        expect(auxpage.errosById(`${id}`)).to.eventually.equal(`Número de Erros: ${perguntas}`);
+    });
 });
--- gui/e2e/step_definitions/noErrosTurma/noErrosTurma.po.ts
+++ gui/e2e/step_definitions/noErrosTurma/noErrosTurma.po.ts
@@ -3,42 +3,41 @@ import { Http } from '@angular/http';
 import { Turma } from '../../../src/app/shared/turma.model';
 
 export class AuxPage {
-  private serverURL = "http://localhost:3000";
-  private guiURL = "http://localhost:4200";
-  
+  private serverURL = 'http://localhost:3000';
+  private guiURL = 'http://localhost:4200';
+
   public navigateTo(page: string) {
-    return browser.get(this.guiURL + "/" + page);
+    browser.get('http://localhost:4200/populate');
+    return browser.get(this.guiURL + '/' + page);
   }
 
-  public logAtTurmaPageAs(cpf: string) {
-    return browser.get(this.guiURL + "/turmas?cpf=" + cpf);
+  public logAs(cpf: string) {
+    return browser.getCurrentUrl()
+      .then(url => browser.get(`${url}?cpf=${cpf}`));
   }
 
   public getTurmas(turmaId): Turma[] {
-    browser.get(this.serverURL + "/turma/" + turmaId)
-    .then(() => {
-      browser.getPageSource()
-      .then(function(res) {
-        var bodyHtml = /<body.*?([\s\S]*)<body>/.exec(res)[1];
-        console.log("GIGG " + JSON.parse(bodyHtml));
+    browser.get(this.serverURL + '/turma/' + turmaId)
+      .then(() => {
+        browser.getPageSource()
+          .then(function (res) {
+            const bodyHtml = /<body.*?([\s\S]*)<body>/.exec(res)[1];
+            console.log('GIGG ' + JSON.parse(bodyHtml));
+          });
       });
-    });
-    return null; 
+    return null;
   }
 
-  // public getAutoAvaliacaoLink() {
-  //   return element(by.id('autoAvaliacaoLink'));
-  // }
+  public acessoTurma(turma: string) {
+    return browser.getCurrentUrl()
+      .then(url => browser.get(url.replace('s?', '-home?id=IF688&')));
+  }
 
-  // public getTHs() {
-  //   return element.all(by.tagName('th'));
-  // }
+  public estatisticasButton() {
+    return element(by.css('html > app-turma-home > div > div > app-gerenciar-turma > div > button:nth-child(2)'));
+  }
 
-  // public getG0(name: string) {
-  //   return element(by.id(`${name}G0`));
-  // }
-
-  // public getG1(name: string) {
-  //   return element(by.id(`${name}G1`));
-  // }
+  public errosById(id: string) {
+    return element(by.id(id)).element(by.className('erros')).getText();
+  }
 }
