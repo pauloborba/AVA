a4f9220d4e129b31cff41f2dde36fae584d16b76
mvgmb <mvgmb@cin.ufpe.br>
2018-12-11 21:57:13 -0300

    Added tests to the Feature Numero de duvidas por turma

--- gui/e2e/step_definitions/noPerguntasTurma/noPerguntasTurma.po.ts
+++ gui/e2e/step_definitions/noPerguntasTurma/noPerguntasTurma.po.ts
@@ -3,27 +3,42 @@ import { Http } from '@angular/http';
 import { Turma } from '../../../src/app/shared/turma.model';
 
 export class AuxPage {
-  private serverURL = "http://localhost:3000";
-  private guiURL = "http://localhost:4200";
-  
+  private serverURL = 'http://localhost:3000';
+  private guiURL = 'http://localhost:4200';
+
   public navigateTo(page: string) {
-    return browser.get(this.guiURL + "/" + page);
+    browser.get('http://localhost:4200/populate');
+    return browser.get(this.guiURL + '/' + page);
   }
 
-  public logAtTurmaPageAs(cpf: string) {
-    return browser.get(this.guiURL + "/turmas?cpf=" + cpf);
+  public logAs(cpf: string) {
+    return browser.getCurrentUrl()
+      .then(url => browser.get(`${url}?cpf=${cpf}`));
   }
 
   public getTurmas(turmaId): Turma[] {
-    browser.get(this.serverURL + "/turma/" + turmaId)
-    .then(() => {
-      browser.getPageSource()
-      .then(function(res) {
-        var bodyHtml = /<body.*?([\s\S]*)<body>/.exec(res)[1];
-        console.log("GIGG " + JSON.parse(bodyHtml));
+    browser.get(this.serverURL + '/turma/' + turmaId)
+      .then(() => {
+        browser.getPageSource()
+          .then(function (res) {
+            const bodyHtml = /<body.*?([\s\S]*)<body>/.exec(res)[1];
+            console.log('GIGG ' + JSON.parse(bodyHtml));
+          });
       });
-    });
-    return null; 
+    return null;
+  }
+
+  public acessoTurma(turma: string) {
+    return browser.getCurrentUrl()
+      .then(url => browser.get(url.replace('s?', '-home?id=IF688&')));
+  }
+
+  public estatisticasButton() {
+    return element(by.css('html > app-turma-home > div > div > app-gerenciar-turma > div > button:nth-child(2)'));
+  }
+
+  public perguntasById(id: string) {
+    return element(by.id(id)).element(by.className('perguntas')).getText();
   }
 
   // public getAutoAvaliacaoLink() {
